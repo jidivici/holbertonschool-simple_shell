2a1d07315ec4ece9946a2547d227a75d84f66c97
Chihaan <you@example.com>
2026-04-17 16:03:40 +0200

    Update : Added comment for the file executer.c

--- executer.c
+++ executer.c
@@ -1,5 +1,11 @@
 #include "simple_shell.h"
-
+/**
+ * execute - forks and executes a command
+ * @tokens: NULL-terminated array of arguments
+ * @prog_name: name of the shell program for error messages
+ *
+ * Return: Nothing
+ */
 void execute(char **tokens)
 {
 	pid_t pid;
