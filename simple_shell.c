b9e6deb269b9e5ce65103f4eb848986d21a84cb3
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-04-17 10:51:47 +0200

    Refactor: handle parsing in the file parser.c and rename split_line function by parser

--- simple_shell.c
+++ simple_shell.c
@@ -1,37 +1,5 @@
 #include <simple_shell.h>
 /**
- * split_line - Tokenizations of stdin line
- * @line: The stdin
- * 
- * Return : A pointer to the newline
- */
-char **split_line(char* line)
-{
-	int capacity = 16;
-	int i = 0;
-	char **tokens = malloc(sizeof(char*) * capacity);
-	char *token;
-
-	if (!tokens)
-		return (NULL);
-
-	token = strtok(line, " \n");
-
-	while(token)
-	{
-		if (i >= capacity - 1)
-		{
-			capacity *= 2;
-			tokens = realloc(tokens, sizeof(char *) * capacity);
-		}
-		tokens[i] = token;
-		i++;
-		token = strtok(NULL, " \n");
-	}
-	tokens[i] = NULL;
-	return (tokens);
-}
-/**
  * main: Read the code
  * @ac: Argument count
  * @av: Argument value
@@ -59,7 +27,7 @@ int main(int ac, char **av)
 
 	while (getline(&line, &len, stdin) != -1)
 	{
-		cmd = split_line(line);
+		cmd = parser(line);
 		printf("%s", cmd[1]);
 		printf("$ ");
 	}
