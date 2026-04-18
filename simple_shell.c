bafa149c1b2aa28e8f2948e95943dd85fe291852
Chihaan <you@example.com>
2026-04-18 16:35:06 +0200

    Update : Simple logic for task 0.1

--- simple_shell.c
+++ simple_shell.c
@@ -4,44 +4,34 @@
  * @ac: Argument count
  * @av: Argument value
  *
- * Return: status
+ * Return: 0
  */
 int main(int ac, char **av)
 {
 	char *line = NULL;
-	int interactive = isatty(STDIN_FILENO), line_count = 0, status = 0;
+	int interactive = isatty(STDIN_FILENO);
 	size_t len = 0;
 	char **tokens;
 	(void)ac;
 
 	if (interactive)
-		write(1, "$ ", 2);
-	while (getline(&line, &len, stdin) != -1)
+		printf("$ ");
+	
+	while(getline(&line, &len, stdin) != -1)
 	{
-		line_count += 1;
 		tokens = parser(line);
 		if (!tokens[0])
 		{
 			free(tokens);
 			if (interactive)
-				write(1, "$ ", 2);
-			continue;
+        			printf("$ ");
+    			continue;
 		}
-		tokens[0] = resolve_command(tokens[0], av[0], line_count);
-		if (!tokens[0])
-		{
-			free(tokens);
-			if (interactive)
-				write(1, "$ ", 2);
-			status = 127;
-			continue;
-		}
-		status = execute(tokens, av[0], line_count);
-		free(tokens[0]);
+		execute(tokens, av[0]);
 		free(tokens);
 		if (interactive)
-			write(1, "$ ", 2);
+			printf("$ ");
 	}
 	free(line);
-	return (status);
+	return (0);
 }
