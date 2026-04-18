2ac86dc4d3356abc39d31d9fd25a8ade598221aa
Chihaan <you@example.com>
2026-04-18 14:17:30 +0200

    Update : Handle error command not found

--- simple_shell.c
+++ simple_shell.c
@@ -8,17 +8,17 @@
  */
 int main(int ac, char **av)
 {
-	char *line = NULL;
-	int interactive = isatty(STDIN_FILENO);
+	char *line = NULL, *cmd_name;
+	int interactive = isatty(STDIN_FILENO), line_count = 0;
 	size_t len = 0;
 	char **tokens;
 	(void)ac;
-	(void)av;
 
 	if (interactive)
 		write(1, "$ ", 2);
 	while (getline(&line, &len, stdin) != -1)
 	{
+		line_count += 1;
 		tokens = parser(line);
 		if (!tokens[0])
 		{
@@ -29,16 +29,20 @@ int main(int ac, char **av)
 		}
 		if (tokens[0][0] != '/')
 		{
+			cmd_name = strdup(tokens[0]);
 			tokens[0] = build_path(tokens[0]);
 			if (!tokens[0])
 			{
+				dprintf(STDERR_FILENO, "%s: line %d: %s: command not found\n",
+					av[0], line_count, cmd_name);
 				free(tokens);
+				free(cmd_name);
 				if (interactive)
 					write(1, "$ ", 2);
 				continue;
 			}
 		}
-		execute(tokens);
+		execute(tokens, av[0], line_count);
 		free(tokens);
 		if (interactive)
 			write(1, "$ ", 2);
